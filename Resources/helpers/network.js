7b6f04e7e3523093733e84146a1643e800b2284e
Zak Fisher <zak.fisher@gmail.com>
2012-10-09 20:36:11 -0700

    added network js class

---
+++ Resources/helpers/network.js
@@ -0,0 +1,49 @@
+/**
+ * Network Helper Class
+ * ====================
+ * Create and send/receive data via HTTP request
+ * ------------------------------------------
+ * by Zachary Fisher :: zfisher@zfidesign.com
+ */
+
+Network = {
+   createRequest : function(url, callback, data) {
+      var requestInitTime = new Date();
+
+      // Create new HTTP Request
+      var request = Ti.Network.createHTTPClient({
+
+         // Success
+         onload: function() {
+            Ti.API.info("Status: " + this.status);
+            var json = JSON.parse(this.responseText);
+            callback.call(this, json);
+
+            // Total Request Time
+            var responseInitTime = new Date();
+            responseInitTime -= requestInitTime;
+            //console.log('response time: ' + responseInitTime);
+         },
+
+         // Fail
+         onerror: function() {
+            //alert('Network Error');
+            return false;
+         },
+
+         // Connection Timeout
+         timeout : 10000
+      });
+
+      // Open + Send Request
+      if (typeof data !== 'undefined') {
+         request.open('POST', url);
+         request.setRequestHeader("Content-Type", "application/json");
+         request.send(data);
+      }
+      else {
+         request.open('GET', url);
+         request.send();
+      }
+   }
+};
